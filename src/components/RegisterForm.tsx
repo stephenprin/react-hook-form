c0280cb465d94fecd7cc383061fc70148ae36c9e
Prince Nmezi <stephenprince427@gmail.com>
2023-07-24 20:42:33 +0100

    form validation added

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,38 +1,65 @@
-import { useForm } from 'react-hook-form'
-import {DevTool} from "@hookform/devtools"
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 type IFormValues = {
   username: string;
   email: string;
-  channel: string
-}
+  channel: string;
+};
 
 export const RegisterForm = () => {
-  const form = useForm<IFormValues>()
-  const { register, control, handleSubmit } = form;
+  const form = useForm<IFormValues>();
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+
   // const {name, ref, onChange, onBlur}= register("username") optional
-  const onSubmit = (data:IFormValues) => { 
-    console.log("form submit", data)
-  }
+  const onSubmit = (data: IFormValues) => {
+    console.log("form submit", data);
+  };
 
   return (
-
     <div>
       <h1>YouTube Form</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className="orm-control">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", { required: "Kindly enter Username" })}
+          />
+          <p className="error">{errors.username?.message}</p>
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              required: "Kindly enter email",
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*＋/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
+              },
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
 
-        <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email")} />
-
-        <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel"  {...register("channel")}/>
-
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", { required: "Kindly enter a channel" })}
+          />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
         <button>Submit</button>
       </form>
-      <DevTool control={control } />
+      <DevTool control={control} />
     </div>
   );
 };
